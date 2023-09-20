aecbe83da4fe65be1274505c8368a0d3e6bca5f2
Sanjai003 <134788995+Sanjai003@users.noreply.github.com>
2023-09-20 21:04:15 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,9 @@
+function clicked() {
+    const name = document.getElementById('name').value;
+    const numb =document.getElementById('password').value;
+     
+    sessionStorage.setItem("name",result-name);
+    sessionStorage.setItem("number",result-numb);
+    return;
+
+}
